cfc05bafaa8c3d36fca7a9973425400e6740f588
zjwalt (MacBook) <zachjw03@gmail.com>
2026-08-10 23:44:12 -0400

    Established the skeleton of abstractions needed to connect to dbs, execute queries and display results

---
+++ src/drivers/base.py
@@ -0,0 +1,82 @@
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
+## --------------- ##
+#   Data Classes    #
+## --------------- ##
+
+
+@dataclass
+class ConnectionConfig:
+    host: str
+    port: int
+    user: str
+    password: str
+    database: str
+
+
+@dataclass
+class ColumnInfo:
+    name: str
+    data_type: str
+    nullable: str
+    primary_key: bool
+    ## Think about Foreign Key relations
+
+
+@dataclass
+class TableInfo:
+    name: str
+    schema: str | None
+    columns: list[ColumnInfo]
+
+
+@dataclass
+class QueryResults:
+    columns: list[str]
+    rows: list[tuple]
+    row_count: int
+
+
+## --------------- ##
+# Custom Exceptions #
+## --------------- ##
+
+
+class DriverError(Exception):
+    """Base for all driver Errors"""
+
+
+class DriverConnectionError(DriverError):
+    """Raised when connect() fails"""
+
+
+class DriverQueryError(DriverError):
+    """Raised when execute_query() fails"""
+
+
+## --------------- ##
+#    Abstraction    #
+## --------------- ##
+
+
+class DatabaseDriver(ABC):
+    @abstractmethod
+    async def connect(self, ConnectionConfig) -> None:
+        """Establish Connection. Raise DriverConnectionError on failure."""
+
+    @abstractmethod
+    async def disconnect(self) -> None:
+        pass
+
+    @abstractmethod
+    async def list_schema(self) -> list[str]:
+        pass
+
+    @abstractmethod
+    async def list_tables(self, schema: str | None) -> list[TableInfo]:
+        pass
+
+    @abstractmethod
+    async def execute_query(self, sql: str) -> QueryResults:
+        """Raise DriverQueryError on failure."""
