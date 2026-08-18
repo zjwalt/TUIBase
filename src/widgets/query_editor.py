cbffd3cf44e20d39a47b862ae08f315041c1b299
zjwalt (MacBook) <zachjw03@gmail.com>
2026-08-18 19:31:33 -0400

    Started working on Query Editor section

--- src/widgets/query_editor.py
+++ src/widgets/query_editor.py
@@ -1,10 +1,15 @@
 from textual.app import ComposeResult
 from textual.widget import Widget
-from textual.widgets import Placeholder
+from textual.widgets import Placeholder, TextArea
+
+TEXT = """\
+SELECT * FROM players;
+"""
 
 
 class QueryEditor(Widget):
     BORDER_TITLE = "Query Editor"
 
     def compose(self) -> ComposeResult:
-        yield Placeholder("Query Editor")
+        yield TextArea.code_editor(TEXT, language="sql")
+        # yield Placeholder("Query Editor")
