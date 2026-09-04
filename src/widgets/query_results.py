34bb5e41ae32f5702b81b9adaf8cc5a496ceeb2e
zjwalt (MacBook) <zachjw03@gmail.com>
2026-09-04 01:28:01 -0400

    Added custom keybinds to results pane

--- src/widgets/query_results.py
+++ src/widgets/query_results.py
@@ -1,10 +1,38 @@
 from textual.app import ComposeResult
 from textual.widget import Widget
 from textual.widgets import DataTable, Label
+from textual import events
 
 
 class QueryResults(Widget):
     BORDER_TITLE = "Query Results"
+
+    VIM_BINDINGS = {
+        "h": "cursor_left",
+        "j": "cursor_down",
+        "k": "cursor_up",
+        "l": "cursor_right",
+    }
+
+    def __init__(self):
+        super().__init__()
+        self.table = None
+
+    def cursor_left(self):
+        current_column = self.table.cursor_column
+        self.table.move_cursor(column=current_column - 1)
+
+    def cursor_down(self):
+        current_row = self.table.cursor_row
+        self.table.move_cursor(row=current_row + 1)
+
+    def cursor_up(self):
+        current_row = self.table.cursor_row
+        self.table.move_cursor(row=current_row - 1)
+
+    def cursor_right(self):
+        current_column = self.table.cursor_column
+        self.table.move_cursor(column=current_column + 1)
 
     def compose(self) -> ComposeResult:
         yield DataTable()
@@ -13,6 +41,13 @@ class QueryResults(Widget):
         self.mount(Label(message))
 
     def show_results(self, columns, values):
-        table = self.query_one(DataTable)
-        table.add_columns(*columns)
-        table.add_rows(values)
+        self.table = self.query_one(DataTable)
+        self.table.clear(columns=True)
+        self.table.add_columns(*columns)
+        self.table.add_rows(values)
+
+    def _on_key(self, event: events.Key):
+        action_name = self.VIM_BINDINGS.get(event.key)
+        if action_name is not None:
+            method = getattr(self, action_name)
+            method()
